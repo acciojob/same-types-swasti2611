43cb3e3cbaa38d4b0511c00c4044588ed7dde583
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-30 21:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ isSameType(NaN, NaN); // true
 isSameType("3", 3); // false
 }
 
-// do not change the code below.
-// let value1 = prompt("Enter Start of the Range.");
-// let value2 = prompt("Enter End Of the Range.");
-// alert(isSameType(value1, value2));
+do not change the code below.
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
+alert(isSameType(value1, value2));
